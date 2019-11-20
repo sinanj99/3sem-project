c4fb5352d1603afe6069a71ec45e92a1fa1bd1c6
Sinan <sinanjasar@live.dk>
2019-11-20 12:38:30 +0100

    first commit

--- src/main/java/rest/ApplicationConfig.java
+++ src/main/java/rest/ApplicationConfig.java
@@ -1,5 +1,7 @@
 package rest;
 
+import io.swagger.v3.jaxrs2.integration.resources.AcceptHeaderOpenApiResource;
+import io.swagger.v3.jaxrs2.integration.resources.OpenApiResource;
 import java.util.Set;
 import javax.ws.rs.core.Application;
 
@@ -10,6 +12,9 @@ public class ApplicationConfig extends Application {
     public Set<Class<?>> getClasses() {
         Set<Class<?>> resources = new java.util.HashSet<>();
         addRestResourceClasses(resources);
+        
+        resources.add(OpenApiResource.class);
+        resources.add(AcceptHeaderOpenApiResource.class);
         return resources;
     }
 
@@ -24,11 +29,7 @@ public class ApplicationConfig extends Application {
         resources.add(cors.CorsResponseFilter.class);
         resources.add(errorhandling.GenericExceptionMapper.class);
         resources.add(org.glassfish.jersey.server.wadl.internal.WadlResource.class);
-        resources.add(rest.ServerResource.class);
-        resources.add(rest.UserResource.class);
-        resources.add(security.JWTAuthenticationFilter.class);
-        resources.add(security.LoginEndpoint.class);
-        resources.add(security.RolesAllowedFilter.class);
+        resources.add(rest.WeatherResource.class);
     }
     
 }
---
+++ src/main/java/rest/WeatherResource.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rest;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import facades.WeatherFacade;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
+import javax.persistence.EntityManagerFactory;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import utils.EMF_Creator;
+
+/**
+ *
+ * @author sinanjasar
+ */
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Weather API",
+                version = "1.0",
+                description = "API to get weather info.",
+                contact = @Contact(name = "Obaydah & Sinan", email = "sinanjasar@live.dk")
+        ),
+        tags = {
+            @Tag(name = "server", description = "API related to person Info")
+
+        },
+        servers = {
+            @Server(
+                    description = "For Local host testing",
+                    url = "http://localhost:8080/3sem-project"
+            ),
+            @Server(
+                    description = "Weather API",
+                    url = "https://sinanjasar.dk/3sem-project"
+            )
+
+        }
+)
+@Path("server")
+public class WeatherResource {
+
+    private static final EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory(
+            "pu",
+            "jdbc:mysql://localhost:3307/startcode",
+            "dev",
+            "ax2",
+            EMF_Creator.Strategy.CREATE);
+    private static final WeatherFacade FACADE = WeatherFacade.getServerFacade(EMF);
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+
+    @Path("all")
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public void getDataFromServers() {
+//        return FACADE.fetchFromServers();
+    }
+
+}
