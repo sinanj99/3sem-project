3ff4c1e5b3b724c8f7f3eea923e461f17de7ba07
Sinan <sinanjasar@live.dk>
2019-11-21 11:11:55 +0100

    eliminated code redundancy, added documentation

--- src/main/java/facades/WeatherFacade.java
+++ src/main/java/facades/WeatherFacade.java
@@ -13,9 +13,16 @@ import com.google.gson.JsonParser;
 import dto.WeatherInfo;
 import java.io.IOException;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.sql.Time;
+import java.time.Instant;
+import java.time.LocalTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -28,8 +35,7 @@ import java.util.Scanner;
 public class WeatherFacade {
 
     private static WeatherFacade instance;
-    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
-    private final static String APIKEY = "50f8d14b7d8a4c64ba1d5c32c9a3aae4";
+    private final static String APIKEY = "50f8d14b7d8a4c64ba1d5c32c9a3aae4"; //must be read as env variable
 
     //Private Constructor to ensure Singleton
     private WeatherFacade() {
@@ -45,77 +51,36 @@ public class WeatherFacade {
         }
         return instance;
     }
+
     /**
-    * Returns a Map containing the temperature for the next 24 hours
-    * @param city
-    * @return Map<String, Integer>
-    * @throws ProtocolException
-    * @throws IOException
-    */
-    private static Map<String, Integer> getHourlyTemp(String city) throws ProtocolException, IOException {
-        URL url = new URL("https://api.weatherbit.io/v2.0/forecast/hourly?hours=24&city="+city+"&key="+APIKEY);
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        con.setRequestMethod("GET");
-        con.setRequestProperty("Accept", "application/json;charset=UTF-8");
-        String jsonStr = "";
-        try (Scanner scan = new Scanner(con.getInputStream())) {
-            while (scan.hasNext()) {
-                jsonStr += scan.nextLine()+"\n";
-            }
-        }
-        
+     * Returns a Map containing the temperature for the next 24 hours
+     *
+     * @param city
+     * @return Map<String, Integer>
+     * @throws ProtocolException
+     * @throws IOException
+     */
+    private Map<String, Integer> getHourlyTemp(String city) throws ProtocolException, IOException {
+        URL url = new URL("https://api.weatherbit.io/v2.0/forecast/hourly?hours=24&city=" + city + "&key=" + APIKEY);
+        String jsonStr = retrieveData(url);
         Map<String, Integer> hourlyTempMap = new LinkedHashMap<>();
         JsonArray hours = new JsonParser().parse(jsonStr)
                 .getAsJsonObject().get("data").getAsJsonArray();
         for (JsonElement hour : hours) {
             String key = hour.getAsJsonObject().get("datetime").getAsString();
-            hourlyTempMap.put(key.substring(key.length()-2), hour.getAsJsonObject().get("temp").getAsInt());            
+            hourlyTempMap.put(key.substring(key.length() - 2), hour.getAsJsonObject().get("temp").getAsInt());
         }
         return hourlyTempMap;
     }
 
-//    /**
-//     * Should call fetchFromServer method on multiple servers
-//     * 
-//     * @param servers
-//     * @return A list containing all json objects returned by servers.
-//     */
-//    public List<Post> fetchFromServers(String[] servers) {
-//        ExecutorService executor = Executors.newFixedThreadPool(5);
-//        Queue<Future> futureList = new LinkedList();
-//        List<Post> posts = new ArrayList();
-//        for (String s : servers) {
-//            Future<Post> future = executor.submit(() -> {
-//                JsonObject jsonObj = new JsonParser().parse(fetchFromServer(s))
-//                        .getAsJsonObject();
-//                return new Post(jsonObj.get("id").getAsInt(),
-//                        jsonObj.get("title").getAsString(), jsonObj.get("body").getAsString());
-//            });
-//            futureList.add(future);
-//        }
-//        while (!futureList.isEmpty()) {
-//            Future<Post> f = futureList.poll();
-//            if (f.isDone()) {
-//                try {
-//                    posts.add(f.get());
-//                } catch (InterruptedException | ExecutionException ex) {
-//                    System.out.println(ex.getMessage());
-//                }
-//            } else {
-//                futureList.add(f);
-//            }
-//        }
-//        return posts;
-//    }
     /**
-     * Returns a list of seven WeatherInfo objects in order
-     * @param city
-     * @return JsonArray
+     * Returns json from the specified URL
+     * @param url
+     * @return json String
      * @throws ProtocolException
-     * @throws IOException
+     * @throws IOException 
      */
-    public static List<WeatherInfo> get7DayForecast(String city) throws ProtocolException, IOException {
-        URL url = new URL("https://api.weatherbit.io/v2.0/forecast/daily?days=7&key=" + APIKEY + "&city=" + city);
+    private String retrieveData(URL url) throws ProtocolException, IOException {
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
         con.setRequestMethod("GET");
         con.setRequestProperty("Accept", "application/json;charset=UTF-8");
@@ -125,6 +90,21 @@ public class WeatherFacade {
                 jsonStr += scan.nextLine();
             }
         }
+        return jsonStr;
+    }
+
+    /**
+     * Returns a list of 7 WeatherInfo objects representing the weather for the
+     * upcoming week.
+     * @param city
+     * @return JsonArray
+     * @throws java.net.MalformedURLException
+     * @throws ProtocolException
+     * @throws IOException
+     */
+    public List<WeatherInfo> get7DayForecast(String city) throws MalformedURLException, IOException {
+        URL url = new URL("https://api.weatherbit.io/v2.0/forecast/daily?days=7&key=" + APIKEY + "&city=" + city);
+        String jsonStr = retrieveData(url);
         List<WeatherInfo> weatherList = new ArrayList();
         JsonArray allDays = new JsonParser().parse(jsonStr)
                 .getAsJsonObject().get("data").getAsJsonArray();
@@ -132,5 +112,6 @@ public class WeatherFacade {
             weatherList.add(new WeatherInfo(day.getAsJsonObject()));
         }
         return weatherList;
+
     }
 }
